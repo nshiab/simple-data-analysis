cef06c45210283257e9eb2981c3f050520c495ce
Nael Shiab <nael.shiab@gmail.com>
2022-07-08 11:47:00 -0400

    Close #67

--- test/unit/methods/importing/loadDataFromUrlNode.test.ts
+++ test/unit/methods/importing/loadDataFromUrlNode.test.ts
@@ -7,8 +7,40 @@ describe("loadDataFromUrlNode", function () {
             "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.csv"
         )
         assert.deepEqual(data, [
-            { patate: 1, poil: 2 },
-            { patate: 3, poil: "coucou" },
+            {
+                patate: 1,
+                poil: 2,
+            },
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
+            {
+                patate: "brioche",
+                poil: "croissant",
+            },
+        ])
+    })
+
+    it("should return an array of objects from a csv file with specific items included", async function () {
+        const data = await loadDataFromUrlNode(
+            "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.csv",
+            1,
+            2
+        )
+        assert.deepEqual(data, [
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
         ])
     })
 
@@ -17,8 +49,40 @@ describe("loadDataFromUrlNode", function () {
             "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.tsv"
         )
         assert.deepEqual(data, [
-            { patate: 1, poil: 2 },
-            { patate: 3, poil: "coucou" },
+            {
+                patate: 1,
+                poil: 2,
+            },
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
+            {
+                patate: "brioche",
+                poil: "croissant",
+            },
+        ])
+    })
+
+    it("should return an array of objects from a tsv file with specific items included", async function () {
+        const data = await loadDataFromUrlNode(
+            "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.tsv",
+            1,
+            2
+        )
+        assert.deepEqual(data, [
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
         ])
     })
 
@@ -27,8 +91,40 @@ describe("loadDataFromUrlNode", function () {
             "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.json"
         )
         assert.deepEqual(data, [
-            { patate: 1, poil: 2 },
-            { patate: 3, poil: "coucou" },
+            {
+                patate: 1,
+                poil: 2,
+            },
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
+            {
+                patate: "brioche",
+                poil: "croissant",
+            },
+        ])
+    })
+
+    it("should return an array of objects from a json file with specific items included", async function () {
+        const data = await loadDataFromUrlNode(
+            "https://raw.githubusercontent.com/nshiab/simple-data-analysis/main/test/unit/methods/importing/testData/localFileTest.json",
+            1,
+            2
+        )
+        assert.deepEqual(data, [
+            {
+                patate: 3,
+                poil: "coucou",
+            },
+            {
+                patate: 8,
+                poil: 10,
+            },
         ])
     })
 })
