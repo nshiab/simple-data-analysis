5a6ed52a393dd103db70b9e98b82b71cf34757f9
Nael Shiab <nael.shiab@gmail.com>
2022-07-19 15:56:24 -0400

    Fixing typos

--- test/unit/methods/restructuring/keysToValues.test.ts
+++ test/unit/methods/restructuring/keysToValues.test.ts
@@ -5,7 +5,7 @@ describe("keysToValues", function () {
     it("should take the values associated with keys and create two new keys: one for the keys and one for the values ", function () {
         const data = [
             {
-                department: "accounting",
+                Departement: "accounting",
                 "2015": 10,
                 "2016": 9,
                 "2017": 15,
@@ -14,7 +14,7 @@ describe("keysToValues", function () {
                 "2020": 32,
             },
             {
-                department: "R&D",
+                Departement: "R&D",
                 "2015": 1,
                 "2016": 2,
                 "2017": 5,
@@ -23,7 +23,7 @@ describe("keysToValues", function () {
                 "2020": 3,
             },
             {
-                department: "sales",
+                Departement: "sales",
                 "2015": 2,
                 "2016": 7,
                 "2017": 15,
@@ -41,24 +41,24 @@ describe("keysToValues", function () {
         )
 
         assert.deepStrictEqual(newData, [
-            { department: "accounting", year: "2015", nbEmployees: 10 },
-            { department: "accounting", year: "2016", nbEmployees: 9 },
-            { department: "accounting", year: "2017", nbEmployees: 15 },
-            { department: "accounting", year: "2018", nbEmployees: 11 },
-            { department: "accounting", year: "2019", nbEmployees: 25 },
-            { department: "accounting", year: "2020", nbEmployees: 32 },
-            { department: "R&D", year: "2015", nbEmployees: 1 },
-            { department: "R&D", year: "2016", nbEmployees: 2 },
-            { department: "R&D", year: "2017", nbEmployees: 5 },
-            { department: "R&D", year: "2018", nbEmployees: 2 },
-            { department: "R&D", year: "2019", nbEmployees: 2 },
-            { department: "R&D", year: "2020", nbEmployees: 3 },
-            { department: "sales", year: "2015", nbEmployees: 2 },
-            { department: "sales", year: "2016", nbEmployees: 7 },
-            { department: "sales", year: "2017", nbEmployees: 15 },
-            { department: "sales", year: "2018", nbEmployees: 32 },
-            { department: "sales", year: "2019", nbEmployees: 45 },
-            { department: "sales", year: "2020", nbEmployees: 27 },
+            { Departement: "accounting", year: "2015", nbEmployees: 10 },
+            { Departement: "accounting", year: "2016", nbEmployees: 9 },
+            { Departement: "accounting", year: "2017", nbEmployees: 15 },
+            { Departement: "accounting", year: "2018", nbEmployees: 11 },
+            { Departement: "accounting", year: "2019", nbEmployees: 25 },
+            { Departement: "accounting", year: "2020", nbEmployees: 32 },
+            { Departement: "R&D", year: "2015", nbEmployees: 1 },
+            { Departement: "R&D", year: "2016", nbEmployees: 2 },
+            { Departement: "R&D", year: "2017", nbEmployees: 5 },
+            { Departement: "R&D", year: "2018", nbEmployees: 2 },
+            { Departement: "R&D", year: "2019", nbEmployees: 2 },
+            { Departement: "R&D", year: "2020", nbEmployees: 3 },
+            { Departement: "sales", year: "2015", nbEmployees: 2 },
+            { Departement: "sales", year: "2016", nbEmployees: 7 },
+            { Departement: "sales", year: "2017", nbEmployees: 15 },
+            { Departement: "sales", year: "2018", nbEmployees: 32 },
+            { Departement: "sales", year: "2019", nbEmployees: 45 },
+            { Departement: "sales", year: "2020", nbEmployees: 27 },
         ])
     })
 })
