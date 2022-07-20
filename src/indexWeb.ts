ddc4e706572c0da26137da8cc37d5a3dfed2813c
Adam Nyx <adam.nyx@cbc.ca>
2022-07-20 15:17:55 -0400

    use default exports for @rollup => indexWeb.ts

--- src/indexWeb.ts
+++ src/indexWeb.ts
@@ -1,1 +1,2 @@
-export { SimpleData } from "./class/index.js"
+// export { SimpleData } from "./class/index.js" // @rollup issue with named exports
+export { default as SimpleData } from "./class/SimpleData.js"
