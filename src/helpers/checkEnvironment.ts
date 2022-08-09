ff1f5408c8ce0a046ba9f6bed28018b0d965f056
Nael Shiab <nael.shiab@gmail.com>
2022-08-09 09:26:47 -0400

    testing for observable notebooks

--- src/helpers/checkEnvironment.ts
+++ src/helpers/checkEnvironment.ts
@@ -1,4 +1,5 @@
 export default function checkEnvironment() {
+    console.log(process, typeof process)
     if (
         typeof process !== "undefined" &&
         process.release.name.search(/node|io.js/) !== -1
