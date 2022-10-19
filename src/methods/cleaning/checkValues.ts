2b741038df7d6f4a931d047ad07c829956c00b70
Nael Shiab <nael.shiab@gmail.com>
2022-10-19 16:04:56 -0400

    104 checkvalues and empty strings (#105)
    * checkValues now identifies empty strings
    
    * allTests run

--- src/methods/cleaning/checkValues.ts
+++ src/methods/cleaning/checkValues.ts
@@ -65,7 +65,13 @@ export default function checkValues(
                 if (!isNaN(array[i] as number)) {
                     typeOf = "date"
                 } else {
-                    typeOf = "invalid date"
+                    typeOf = "invalidDate"
+                }
+            } else if (typeof array[i] === "string") {
+                if (array[i] === "") {
+                    typeOf = "emptyString"
+                } else {
+                    typeOf = "string"
                 }
             } else {
                 typeOf = typeof array[i]
@@ -79,7 +85,7 @@ export default function checkValues(
         }
 
         for (const key of Object.keys(checks)) {
-            if (key !== "key" && key != "count" && checks[key] !== 0) {
+            if (key !== "key" && key != "count") {
                 checks[key] = `${checks[key]} | ${toPercentage(
                     checks[key] as number,
                     checks.count,
