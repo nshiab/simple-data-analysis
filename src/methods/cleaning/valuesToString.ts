ec5cfc7caf0a4029c09012178f5f9cf6e1868ec1
a-nyx <adam.nyx@cbc.ca>
2022-11-01 09:02:23 -0700

    add newKey option for valuesToString

--- src/methods/cleaning/valuesToString.ts
+++ src/methods/cleaning/valuesToString.ts
@@ -3,14 +3,21 @@ import hasKey from "../../helpers/hasKey.js"
 
 export default function valuesToString(
     data: SimpleDataItem[],
-    key: string
+    key: string,
+    newKey?: string
 ): SimpleDataItem[] {
     if (!hasKey(data[0], key)) {
         throw new Error("No key " + key)
     }
 
+    if (newKey && hasKey(data[0], newKey)) {
+        throw new Error(newKey + " already exists")
+    }
+
+    const keyToUpdate = newKey ? newKey : key
+
     for (let i = 0; i < data.length; i++) {
-        data[i][key] = String(data[i][key])
+        data[i][keyToUpdate] = String(data[i][key])
     }
 
     return data
