c7c4ae7855025143dc3f020f6da134648180ae2b
a-nyx <adam.nyx@cbc.ca>
2022-11-01 09:53:28 -0700

    check if newKey alreeady exists

--- src/methods/cleaning/renameKey.ts
+++ src/methods/cleaning/renameKey.ts
@@ -10,6 +10,10 @@ export default function renameKey(
         throw new Error("No key " + oldKey)
     }
 
+    if (newKey && hasKey(data[0], newKey)) {
+        throw new Error(newKey + " already exists")
+    }
+
     for (let i = 0; i < data.length; i++) {
         const d = data[i]
         d[newKey] = d[oldKey]
