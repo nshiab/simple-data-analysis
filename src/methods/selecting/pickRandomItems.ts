fbc1e4b84133626d49aa60b29266e44e9304b9a0
Robert Davidson <robert.davidson@cbc.ca>
2022-11-02 15:45:23 -0400

    Add default value for nbItems, update log

--- src/methods/selecting/pickRandomItems.ts
+++ src/methods/selecting/pickRandomItems.ts
@@ -6,19 +6,19 @@ import { randomLcg } from "d3-random"
 
 export default function pickRandomItems(
     data: SimpleDataItem[],
-    numberOfRandomItems: number,
+    nbItems = 100,
     seed?: number,
     verbose = false
 ): SimpleDataItem[] {
     const shuffle = seed ? shuffler(randomLcg(seed)) : shuffler(randomLcg())
-    const randomizedData = shuffle(data).slice(0, numberOfRandomItems)
-    const nbRemoved = data.length - numberOfRandomItems
+    const randomizedData = shuffle(data).slice(0, nbItems)
+    const nbRemoved = data.length - nbItems
     verbose &&
         log(
             `/!\\ ${nbRemoved} items removed, representing ${toPercentage(
                 nbRemoved,
                 data.length
-            )} of received items. Remaining items randomized in order`,
+            )} of received items. Remaining items randomized in order.`,
             "bgRed"
         )
     return randomizedData
