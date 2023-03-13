2d0617d51d24135ab4298085c38211aa7aa02ad2
Nael Shiab <nael.shiab@gmail.com>
2023-03-13 16:04:32 -0400

    export keep and exclude as functions

--- src/exports/selecting.ts
+++ src/exports/selecting.ts
@@ -2,6 +2,8 @@ import filterItems from "../methods/selecting/filterItems.js"
 import filterValues from "../methods/selecting/filterValues.js"
 import pickRandomItems from "../methods/selecting/pickRandomItems.js"
 import selectKeys from "../methods/selecting/selectKeys.js"
+import keep from "../methods/selecting/keep.js"
+import exclude from "../methods/selecting/exclude.js"
 import keepDates from "../methods/selecting/keepDates.js"
 import keepNumbers from "../methods/selecting/keepNumbers.js"
 import keepStrings from "../methods/selecting/keepStrings.js"
@@ -11,6 +13,8 @@ export {
     filterValues,
     pickRandomItems,
     selectKeys,
+    keep,
+    exclude,
     keepDates,
     keepNumbers,
     keepStrings,
