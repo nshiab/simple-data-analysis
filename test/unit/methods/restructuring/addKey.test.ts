6d8470ebc645b969136236cf095f9d64057f3636
Andrew Ryan <andrew.ryan@cbc.ca>
2023-06-27 15:20:13 -0400

    adds tests for expanded addKey and modifyItems arguments

--- test/unit/methods/restructuring/addKey.test.ts
+++ test/unit/methods/restructuring/addKey.test.ts
@@ -1,5 +1,5 @@
 import assert from "assert"
-import { SimpleData } from "../../../../src/index.js"
+import { SimpleData, SimpleDataValue } from "../../../../src/index.js"
 
 describe("addItems", function () {
     it("should add a new key and generate new values", function () {
@@ -11,6 +11,38 @@ describe("addItems", function () {
         assert.deepStrictEqual(sd.getData(), [
             { key1: 1, key2: 2 },
             { key1: 2, key2: 4 },
+        ])
+    })
+
+    it("should add a new key and generate new values computed from prior values", function () {
+        const data = [{ key1: 1 }, { key1: 2 }, { key1: 3 }, { key1: 1 }]
+        const sd = new SimpleData({ data }).addKey({
+            key: "key2",
+            itemGenerator: (item, i) => (item.key1 as number) * 2 + i,
+        })
+
+        const sd2 = new SimpleData({ data }).addKey({
+            key: "key3",
+            itemGenerator: (item, idx, items) => {
+                return (
+                    (item.key1 as number) * 2 +
+                    idx +
+                    (idx > 0 ? (items[idx - 1].key1 as number) ?? 0 : 0)
+                )
+            },
+        })
+
+        assert.deepStrictEqual(sd.getData(), [
+            { key1: 1, key2: 2 },
+            { key1: 2, key2: 5 },
+            { key1: 3, key2: 8 },
+            { key1: 1, key2: 5 },
+        ])
+        assert.deepStrictEqual(sd2.getData(), [
+            { key1: 1, key3: 2 },
+            { key1: 2, key3: 6 },
+            { key1: 3, key3: 10 },
+            { key1: 1, key3: 8 },
         ])
     })
 })
--- test/unit/methods/restructuring/modifyItems.test.ts
+++ test/unit/methods/restructuring/modifyItems.test.ts
@@ -20,6 +20,7 @@ describe("modifyItems", function () {
             { key1: 8, key2: 4 },
         ])
     })
+
     it("should modify items and adds the new values in a new key", function () {
         const data = [
             { key1: 1, key2: 2 },
@@ -45,6 +46,58 @@ describe("modifyItems", function () {
                 key2: 4,
                 key3: 8,
             },
+        ])
+    })
+
+    it("should modify items based on prior values", function () {
+        const data = [
+            { key1: 1, key2: 2, other: 2 },
+            { key1: 2, key2: 4, other: 4 },
+            { key1: 1, key2: 3, other: 3 },
+            { key1: 4, key2: 1, other: 1 },
+        ]
+
+        const sd = new SimpleData({ data }).modifyItems({
+            key: "key1",
+            itemGenerator: (item, i, items) =>
+                item.key1 && item.key2
+                    ? (item.key1 as number) * (item.key2 as number) +
+                      i +
+                      (items[i - 1] ? (items[i - 1]?.other as number) ?? 0 : 0)
+                    : undefined,
+        })
+        assert.deepStrictEqual(sd.getData(), [
+            { key1: 2, key2: 2, other: 2 },
+            { key1: 11, key2: 4, other: 4 },
+            { key1: 9, key2: 3, other: 3 },
+            { key1: 10, key2: 1, other: 1 },
+        ])
+    })
+
+    it("should modify items and adds the new values in a new key based on prior values", function () {
+        const data = [
+            { key1: 1, key2: 2, other: 2 },
+            { key1: 2, key2: 4, other: 4 },
+            { key1: 1, key2: 3, other: 3 },
+            { key1: 4, key2: 1, other: 1 },
+        ]
+
+        const sd = new SimpleData({ data }).modifyItems({
+            key: "key1",
+            newKey: "key3",
+            itemGenerator: (item, i, items) =>
+                item.key1 && item.key2
+                    ? (item.key1 as number) * (item.key2 as number) +
+                      i +
+                      (items[i - 1] ? (items[i - 1]?.other as number) ?? 0 : 0)
+                    : undefined,
+        })
+
+        assert.deepStrictEqual(sd.getData(), [
+            { key1: 1, key2: 2, other: 2, key3: 2 },
+            { key1: 2, key2: 4, other: 4, key3: 11 },
+            { key1: 1, key2: 3, other: 3, key3: 9 },
+            { key1: 4, key2: 1, other: 1, key3: 10 },
         ])
     })
 })
