9d5f6af059ab748c2b576aa6c73f5f225eb0e710
Nael Shiab <nael.shiab@gmail.com>
2023-11-10 16:32:56 -0500

    Adding writeChart

---
+++ test/unit/methods/writeChart.test.ts
@@ -0,0 +1,38 @@
+import { existsSync, mkdirSync } from "fs"
+import SimpleNodeDB from "../../../src/class/SimpleNodeDB.js"
+import { Data, dot } from "@observablehq/plot"
+
+const outputPath = "./test/output/"
+
+describe("writeChart", () => {
+    let simpleNodeDB: SimpleNodeDB
+    before(async function () {
+        if (!existsSync(outputPath)) {
+            mkdirSync(outputPath)
+        }
+
+        simpleNodeDB = await new SimpleNodeDB().start()
+        await simpleNodeDB.loadData("employees", "test/data/employees.csv")
+        await simpleNodeDB.convert(
+            "employees",
+            { Salary: "bigint" },
+            { try: true }
+        )
+    })
+    after(async function () {
+        await simpleNodeDB.done()
+    })
+
+    it("should write a chart with Observable Plot options", async () => {
+        const data = (await simpleNodeDB.getData("employees")) as Data
+        await simpleNodeDB.writeChart(`${outputPath}chart.html`, {
+            x: { type: "point" },
+            marks: [
+                dot(data, {
+                    x: "Department or unit",
+                    y: "Salary",
+                }),
+            ],
+        })
+    })
+})
