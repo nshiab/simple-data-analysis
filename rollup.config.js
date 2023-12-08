10674d3356b1309c2a3348a2f1a3de61eb2a50a4
Nael Shiab <nael.shiab@gmail.com>
2023-12-07 19:41:36 -0500

    new export setup

--- rollup.config.js
+++ rollup.config.js
@@ -15,20 +15,9 @@ const copyright = readFileSync("./LICENSE", "utf-8")
 
 const banner = `// ${meta.homepage} v${meta.version} Copyright ${copyright}`
 
-const commonPlugins = [
-    nodePolyfills(),
-    typescript(),
-    commonjs(),
-    resolve({
-        jsnext: true,
-        main: true,
-        browser: true,
-    }),
-]
-
 export default [
     {
-        input: "src/indexWeb.ts",
+        input: "src/bundle.ts",
         output: {
             file: `dist/${meta.name}.min.js`,
             name: "SimpleDB",
@@ -37,10 +26,12 @@ export default [
             extend: true,
             banner: banner,
             sourcemap: true,
-            // inlineDynamicImports: true,
         },
         plugins: [
-            ...commonPlugins,
+            nodePolyfills(),
+            typescript(),
+            commonjs(),
+            resolve({ browser: true }),
             terser({
                 output: {
                     preamble: banner,
