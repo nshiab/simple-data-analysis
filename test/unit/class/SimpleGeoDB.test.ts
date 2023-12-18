bbf1f5aade86e313ac68676947cdf6a2a990a3b2
Nael Shiab <nael.shiab@gmail.com>
2023-12-18 15:00:32 -0500

    adding SimpleGeoDB to web bundle

--- test/unit/class/SimpleGeoDB.test.ts
+++ test/unit/class/SimpleGeoDB.test.ts
@@ -1,6 +1,5 @@
 import assert from "assert"
 import SimpleGeoDB from "../../../src/class/SimpleGeoDB.js"
-// import { AsyncDuckDB, AsyncDuckDBConnection } from "@duckdb/duckdb-wasm"
 
 describe("SimpleGeoDB", () => {
     const simpleGeoDB = new SimpleGeoDB()
@@ -8,20 +7,5 @@ describe("SimpleGeoDB", () => {
         assert.deepStrictEqual(simpleGeoDB instanceof SimpleGeoDB, true)
     })
 
-    it("should start and instantiate a db", async () => {
-        console.log("NOT WORKING FOR NOW.")
-        // await simpleGeoDB.start()
-        // assert.deepStrictEqual(simpleGeoDB.db instanceof AsyncDuckDB, true)
-    })
-    it("should instantiate a connection", async () => {
-        console.log("NOT WORKING FOR NOW.")
-        // assert.deepStrictEqual(
-        // simpleGeoDB.connection instanceof AsyncDuckDBConnection,
-        // true
-        // )
-    })
-    it("should close the db", async () => {
-        console.log("NOT WORKING FOR NOW.")
-        // await simpleGeoDB.done()
-    })
+    // For the rest, testing in https://observablehq.com/@nshiab/testing-simple-data-analysis-v2-0-0
 })
