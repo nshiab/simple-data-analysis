8cfac9a37b987c2b9420cdb12213a69410e642bc
Nael Shiab <nael.shiab@gmail.com>
2024-01-04 14:48:29 -0500

    typo

--- test/unit/methods/updateColumn.test.ts
+++ test/unit/methods/updateColumn.test.ts
@@ -1,7 +1,7 @@
 import assert from "assert"
 import SimpleNodeDB from "../../../src/class/SimpleNodeDB.js"
 
-describe("update", () => {
+describe("updateColumn", () => {
     let simpleNodeDB: SimpleNodeDB
     before(async function () {
         simpleNodeDB = new SimpleNodeDB()
