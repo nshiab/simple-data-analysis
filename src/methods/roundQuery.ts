b754f87372b5de178624fe55b38bd0815c25b107
Nael Shiab <nael.shiab@gmail.com>
2024-01-09 15:00:42 -0500

    round now works with multiple columns

--- src/methods/roundQuery.ts
+++ src/methods/roundQuery.ts
@@ -3,19 +3,19 @@ export default function roundQuery(
     columns: string[],
     options: { method?: "round" | "ceiling" | "floor"; decimals?: number }
 ) {
-    let query = `UPDATE ${table}`
+    let query = `UPDATE ${table} SET`
     const method = options.method?.toUpperCase() ?? "ROUND"
     const decimals = options.decimals ?? 0
 
     if (method === "ROUND") {
         for (const column of columns) {
-            query += `\nSET "${column}" = ${method}("${column}", ${decimals})`
+            query += `\n"${column}" = ${method}("${column}", ${decimals}),`
         }
     } else {
         for (const column of columns) {
-            query += `\nSET "${column}" = ${method}("${column}")`
+            query += `\n"${column}" = ${method}("${column}"),`
         }
     }
 
-    return query
+    return query.slice(0, query.length - 1)
 }
