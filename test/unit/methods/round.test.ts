4ae5933557732f5b36b6d7dd2d67e24f2d68e039
Nael Shiab <nael.shiab@gmail.com>
2024-01-09 15:01:56 -0500

    round now works with multiple columns (#308)

--- test/unit/methods/round.test.ts
+++ test/unit/methods/round.test.ts
@@ -14,6 +14,7 @@ describe("round", () => {
         await simpleNodeDB.loadData("dataInteger", [
             "test/data/files/dataManyDecimals.csv",
         ])
+        await simpleNodeDB.selectColumns("dataInteger", ["key1"])
 
         await simpleNodeDB.round("dataInteger", ["key1"])
 
@@ -30,7 +31,7 @@ describe("round", () => {
         await simpleNodeDB.loadData("dataDecimals", [
             "test/data/files/dataManyDecimals.csv",
         ])
-
+        await simpleNodeDB.selectColumns("dataDecimals", ["key1"])
         await simpleNodeDB.round("dataDecimals", ["key1"], {
             decimals: 3,
         })
@@ -47,7 +48,7 @@ describe("round", () => {
         await simpleNodeDB.loadData("dataFloor", [
             "test/data/files/dataManyDecimals.csv",
         ])
-
+        await simpleNodeDB.selectColumns("dataFloor", ["key1"])
         await simpleNodeDB.round("dataFloor", ["key1"], {
             method: "floor",
         })
@@ -65,7 +66,7 @@ describe("round", () => {
         await simpleNodeDB.loadData("dataCeil", [
             "test/data/files/dataManyDecimals.csv",
         ])
-
+        await simpleNodeDB.selectColumns("dataCeil", ["key1"])
         await simpleNodeDB.round("dataCeil", ["key1"], {
             method: "ceiling",
         })
@@ -76,6 +77,22 @@ describe("round", () => {
             { key1: 4 },
             { key1: 9 },
             { key1: 10 },
+        ])
+    })
+    it("should round multiple columns", async () => {
+        await simpleNodeDB.loadData("dataMultipleColumns", [
+            "test/data/files/dataManyDecimals.csv",
+        ])
+        await simpleNodeDB.round("dataMultipleColumns", ["key1", "key2"], {
+            decimals: 2,
+        })
+        const data = await simpleNodeDB.getData("dataMultipleColumns")
+
+        assert.deepStrictEqual(data, [
+            { key1: 1.04, key2: 2.95 },
+            { key1: 3.24, key2: 4.99 },
+            { key1: 8.1, key2: 34.5 },
+            { key1: 10, key2: 100 },
         ])
     })
 })
