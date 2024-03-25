16678e74aca77e78c020341b604bd4c543af1dfe
Nael Shiab <nael.shiab@gmail.com>
2024-03-25 16:03:04 -0400

    adding method cloneColumn

---
+++ src/methods/cloneColumn.ts
@@ -0,0 +1,19 @@
+export default function cloneColumnQuery(
+    table: string,
+    originalColumn: string,
+    newColumn: string,
+    types: { [key: string]: string }
+) {
+    let query = ""
+
+    const originalColumnType = types[originalColumn]
+
+    if (originalColumnType) {
+        query += `ALTER TABLE ${table} ADD COLUMN "${newColumn}" ${originalColumnType};
+        UPDATE ${table} SET "${newColumn}" = "${originalColumn}"`
+    } else {
+        throw new Error(`Can't find type of ${originalColumn}`)
+    }
+
+    return query
+}
