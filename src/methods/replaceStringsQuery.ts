34e2a4335a886101c6fa3eda7520903e8c855d7c
Nael Shiab <nael.shiab@gmail.com>
2024-03-27 11:47:23 -0400

    option to use regex with replace

--- src/methods/replaceStringsQuery.ts
+++ src/methods/replaceStringsQuery.ts
@@ -3,7 +3,7 @@ export default function replaceStringsQuery(
     columns: string[],
     oldTexts: string[],
     newTexts: string[],
-    options: { entireString?: boolean } = {}
+    options: { entireString?: boolean; regex?: boolean } = {}
 ) {
     let query = ""
 
@@ -18,6 +18,8 @@ export default function replaceStringsQuery(
                     WHEN "${column}" = '${oldTexts[i]}' THEN '${newTexts[i]}'
                     ELSE "${column}"
                 END;\n`
+            } else if (options.regex) {
+                query += `UPDATE ${table} SET "${column}" = REGEXP_REPLACE("${column}", '${oldTexts[i]}', '${newTexts[i]}', 'g');\n`
             } else {
                 query += `UPDATE ${table} SET "${column}" = REPLACE("${column}", '${oldTexts[i]}', '${newTexts[i]}');\n`
             }
