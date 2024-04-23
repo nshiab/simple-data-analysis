8a594a9e4d86101ac4c8cd553ec0653fdb66f91a
Nael Shiab <nael.shiab@gmail.com>
2024-04-23 16:50:15 -0400

    SimpleDB just in browser (#445)
    * updating dependencies and removing wasm in node
    
    * 2.13.2-rc.0
    
    * Updating doc
    
    * No need for web-worker dependency anymore

--- src/helpers/getDuckDB.ts
+++ src/helpers/getDuckDB.ts
@@ -1,40 +1,9 @@
 import * as duckdb from "@duckdb/duckdb-wasm"
-import { AsyncDuckDB, DuckDBBundle } from "@duckdb/duckdb-wasm"
-import Worker from "web-worker"
-import path from "path"
+import { AsyncDuckDB } from "@duckdb/duckdb-wasm"
 
 const SilentLogger = { log: () => {} }
 
-type WorkerBundle = { bundle: DuckDBBundle; worker: Worker }
-
-async function nodeWorkerBundle(): Promise<WorkerBundle> {
-    const DUCKDB_DIST = `node_modules/@duckdb/duckdb-wasm/dist`
-    const bundle = await duckdb.selectBundle({
-        mvp: {
-            mainModule: path.resolve(DUCKDB_DIST, "./duckdb-mvp.wasm"),
-            mainWorker: path.resolve(
-                DUCKDB_DIST,
-                "./duckdb-node-mvp.worker.cjs"
-            ),
-        },
-        eh: {
-            mainModule: path.resolve(DUCKDB_DIST, "./duckdb-eh.wasm"),
-            mainWorker: path.resolve(
-                DUCKDB_DIST,
-                "./duckdb-node-eh.worker.cjs"
-            ),
-        },
-    })
-    const mainWorker = bundle.mainWorker
-    if (mainWorker) {
-        const worker = new Worker(mainWorker)
-        return { bundle, worker }
-    } else {
-        throw Error(`No mainWorker: ${mainWorker}`)
-    }
-}
-
-async function browserWorkerBundle(): Promise<WorkerBundle> {
+async function browserWorkerBundle() {
     const allBundles = duckdb.getJsDelivrBundles()
     const bundle = await duckdb.selectBundle(allBundles)
     const mainWorker = bundle.mainWorker
@@ -50,17 +19,7 @@ export default async function getDuckDB(): Promise<{
     db: AsyncDuckDB
     worker: Worker
 }> {
-    if (typeof window === "undefined") {
-        console.log(
-            "\x1b[41m",
-            "If you are not on a web browser, please use SimpleNodeDB.",
-            "\x1b[0m"
-        )
-    }
-
-    const { worker, bundle } = await (typeof window === "undefined"
-        ? nodeWorkerBundle()
-        : browserWorkerBundle())
+    const { worker, bundle } = await browserWorkerBundle()
     const logger = SilentLogger
     const db = new AsyncDuckDB(logger, worker)
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker)
