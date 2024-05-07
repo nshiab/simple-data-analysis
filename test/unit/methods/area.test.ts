14d5235d2855e8266719a56945bba702c52103ca
Nael Shiab <nael.shiab@gmail.com>
2024-05-07 14:15:13 -0400

    468 using spheroid functions (#469)
    * Done with area
    
    * better example
    
    * Done with distance

--- test/unit/methods/area.test.ts
+++ test/unit/methods/area.test.ts
@@ -5,23 +5,17 @@ describe("area", () => {
     let simpleNodeDB: SimpleNodeDB
     before(async function () {
         simpleNodeDB = new SimpleNodeDB({ spatial: true })
-        await simpleNodeDB.loadGeoData(
-            "geodata",
-            "test/geodata/files/CanadianProvincesAndTerritories.json"
-        )
-        await simpleNodeDB.flipCoordinates("geodata", "geom")
-        await simpleNodeDB.reproject(
-            "geodata",
-            "geom",
-            "EPSG:4326",
-            "EPSG:3347"
-        )
     })
     after(async function () {
         await simpleNodeDB.done()
     })
 
-    it("should calculate the area of geometries", async () => {
+    it("should calculate the area of geometries in square meters", async () => {
+        await simpleNodeDB.loadGeoData(
+            "geodata",
+            "test/geodata/files/CanadianProvincesAndTerritories.json"
+        )
+        await simpleNodeDB.flipCoordinates("geodata", "geom")
         await simpleNodeDB.area("geodata", "geom", "area")
         await simpleNodeDB.selectColumns("geodata", [
             "nameEnglish",
@@ -35,64 +29,123 @@ describe("area", () => {
             {
                 nameEnglish: "Newfoundland and Labrador",
                 nameFrench: "Terre-Neuve-et-Labrador",
-                area: 397993584863,
+                area: 407428312235,
             },
             {
                 nameEnglish: "Prince Edward Island",
                 nameFrench: "Île-du-Prince-Édouard",
-                area: 6054304069,
+                area: 5922205562,
             },
             {
                 nameEnglish: "Nova Scotia",
                 nameFrench: "Nouvelle-Écosse",
-                area: 58911632890,
+                area: 56980046392,
             },
             {
                 nameEnglish: "New Brunswick",
                 nameFrench: "Nouveau-Brunswick",
-                area: 74400254883,
+                area: 72935040140,
             },
             {
                 nameEnglish: "Quebec",
                 nameFrench: "Québec",
-                area: 1474790458654,
+                area: 1508203157285,
             },
             {
                 nameEnglish: "Ontario",
                 nameFrench: "Ontario",
-                area: 973669312293,
+                area: 980252087873,
             },
             {
                 nameEnglish: "Manitoba",
                 nameFrench: "Manitoba",
-                area: 627551804386,
+                area: 649626747368,
             },
             {
                 nameEnglish: "Saskatchewan",
                 nameFrench: "Saskatchewan",
-                area: 632530143909,
+                area: 652741441946,
             },
             {
                 nameEnglish: "Alberta",
                 nameFrench: "Alberta",
-                area: 639689656247,
+                area: 663023544390,
             },
             {
                 nameEnglish: "British Columbia",
                 nameFrench: "Colombie-Britannique",
-                area: 907899872957,
+                area: 938271085581,
             },
-            { nameEnglish: "Yukon", nameFrench: "Yukon", area: 455413471710 },
+            { nameEnglish: "Yukon", nameFrench: "Yukon", area: 483592253984 },
             {
                 nameEnglish: "Northwest Territories",
                 nameFrench: "Territoires du Nord-Ouest",
-                area: 1274701572599,
+                area: 1347423374589,
             },
             {
                 nameEnglish: "Nunavut",
                 nameFrench: "Nunavut",
-                area: 2008939671560,
+                area: 2090913434132,
             },
+        ])
+    })
+    it("should calculate the area of geometries in square kilometers", async () => {
+        await simpleNodeDB.loadGeoData(
+            "geodata",
+            "test/geodata/files/CanadianProvincesAndTerritories.json"
+        )
+        await simpleNodeDB.flipCoordinates("geodata", "geom")
+        await simpleNodeDB.area("geodata", "geom", "area", { unit: "km2" })
+        await simpleNodeDB.selectColumns("geodata", [
+            "nameEnglish",
+            "nameFrench",
+            "area",
+        ])
+        await simpleNodeDB.round("geoData", "area")
+        const data = await simpleNodeDB.getData("geodata")
+
+        assert.deepStrictEqual(data, [
+            {
+                nameEnglish: "Newfoundland and Labrador",
+                nameFrench: "Terre-Neuve-et-Labrador",
+                area: 407428,
+            },
+            {
+                nameEnglish: "Prince Edward Island",
+                nameFrench: "Île-du-Prince-Édouard",
+                area: 5922,
+            },
+            {
+                nameEnglish: "Nova Scotia",
+                nameFrench: "Nouvelle-Écosse",
+                area: 56980,
+            },
+            {
+                nameEnglish: "New Brunswick",
+                nameFrench: "Nouveau-Brunswick",
+                area: 72935,
+            },
+            { nameEnglish: "Quebec", nameFrench: "Québec", area: 1508203 },
+            { nameEnglish: "Ontario", nameFrench: "Ontario", area: 980252 },
+            { nameEnglish: "Manitoba", nameFrench: "Manitoba", area: 649627 },
+            {
+                nameEnglish: "Saskatchewan",
+                nameFrench: "Saskatchewan",
+                area: 652741,
+            },
+            { nameEnglish: "Alberta", nameFrench: "Alberta", area: 663024 },
+            {
+                nameEnglish: "British Columbia",
+                nameFrench: "Colombie-Britannique",
+                area: 938271,
+            },
+            { nameEnglish: "Yukon", nameFrench: "Yukon", area: 483592 },
+            {
+                nameEnglish: "Northwest Territories",
+                nameFrench: "Territoires du Nord-Ouest",
+                area: 1347423,
+            },
+            { nameEnglish: "Nunavut", nameFrench: "Nunavut", area: 2090913 },
         ])
     })
 })
--- test/unit/methods/distance.test.ts
+++ test/unit/methods/distance.test.ts
@@ -10,31 +10,61 @@ describe("distance", () => {
         await simpleNodeDB.done()
     })
 
-    it("should calculate the distance between geometries", async () => {
+    it("should calculate the distance between geometries in meters", async () => {
         await simpleNodeDB.loadData(
             "data",
             "test/geodata/files/coordinates.csv"
         )
         await simpleNodeDB.points("data", "lon", "lat", "geom")
         await simpleNodeDB.flipCoordinates("data", "geom")
-        await simpleNodeDB.reproject("data", "geom", "EPSG:4326", "EPSG:3347")
         await simpleNodeDB.selectColumns("data", ["name", "geom"])
         await simpleNodeDB.cloneTable("data", "dataClone")
         await simpleNodeDB.crossJoin("data", "dataClone")
         await simpleNodeDB.distance("data", "geom", "geom_1", "dist")
         await simpleNodeDB.selectColumns("data", ["name", "name_1", "dist"])
+        await simpleNodeDB.round("data", "dist")
 
         const data = await simpleNodeDB.getData("data")
 
         assert.deepStrictEqual(data, [
             { name: "montreal", name_1: "montreal", dist: 0 },
-            { name: "montreal", name_1: "toronto", dist: 474717.89401638263 },
-            { name: "montreal", name_1: "vancouver", dist: 3375210.3233094704 },
-            { name: "toronto", name_1: "montreal", dist: 474717.89401638263 },
+            { name: "montreal", name_1: "toronto", dist: 465639 },
+            { name: "montreal", name_1: "vancouver", dist: 3360308 },
+            { name: "toronto", name_1: "montreal", dist: 465639 },
             { name: "toronto", name_1: "toronto", dist: 0 },
-            { name: "toronto", name_1: "vancouver", dist: 3675512.5531728626 },
-            { name: "vancouver", name_1: "montreal", dist: 3375210.3233094704 },
-            { name: "vancouver", name_1: "toronto", dist: 3675512.5531728626 },
+            { name: "toronto", name_1: "vancouver", dist: 3676968 },
+            { name: "vancouver", name_1: "montreal", dist: 3360308 },
+            { name: "vancouver", name_1: "toronto", dist: 3676968 },
+            { name: "vancouver", name_1: "vancouver", dist: 0 },
+        ])
+    })
+    it("should calculate the distance between geometries in kilometers", async () => {
+        await simpleNodeDB.loadData(
+            "data",
+            "test/geodata/files/coordinates.csv"
+        )
+        await simpleNodeDB.points("data", "lon", "lat", "geom")
+        await simpleNodeDB.flipCoordinates("data", "geom")
+        await simpleNodeDB.selectColumns("data", ["name", "geom"])
+        await simpleNodeDB.cloneTable("data", "dataClone")
+        await simpleNodeDB.crossJoin("data", "dataClone")
+        await simpleNodeDB.distance("data", "geom", "geom_1", "dist", {
+            unit: "km",
+        })
+        await simpleNodeDB.selectColumns("data", ["name", "name_1", "dist"])
+        await simpleNodeDB.round("data", "dist")
+
+        const data = await simpleNodeDB.getData("data")
+
+        assert.deepStrictEqual(data, [
+            { name: "montreal", name_1: "montreal", dist: 0 },
+            { name: "montreal", name_1: "toronto", dist: 466 },
+            { name: "montreal", name_1: "vancouver", dist: 3360 },
+            { name: "toronto", name_1: "montreal", dist: 466 },
+            { name: "toronto", name_1: "toronto", dist: 0 },
+            { name: "toronto", name_1: "vancouver", dist: 3677 },
+            { name: "vancouver", name_1: "montreal", dist: 3360 },
+            { name: "vancouver", name_1: "toronto", dist: 3677 },
             { name: "vancouver", name_1: "vancouver", dist: 0 },
         ])
     })
