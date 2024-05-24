6cd46623aa56238abe3fe53aea6335066bba00fe
Nael Shiab <nael.shiab@gmail.com>
2024-05-24 12:56:21 -0400

    Updating bundle and index

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,2 +1,2 @@
 export { default as SimpleWebDB } from "./class/SimpleWebDB.js"
-export { default as SimpleGeoDB } from "./class/SimpleGeoDB.js"
+export { default as SimpleWebTable } from "./class/SimpleWebTable.js"
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { default as SimpleWebDB } from "./class/SimpleWebDB.js"
-export { default as SimpleGeoDB } from "./class/SimpleGeoDB.js"
 export { default as SimpleDB } from "./class/SimpleDB.js"
+export { default as SimpleTable } from "./class/SimpleTable.js"
