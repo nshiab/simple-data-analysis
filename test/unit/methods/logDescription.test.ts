316e5cd6978b9406869a4c2f3da8c5fa4afe328c
Nael Shiab <nael.shiab@gmail.com>
2024-06-16 11:50:52 -0400

    new method logDescription (#535)

--- test/unit/methods/logDescription.test.ts
+++ test/unit/methods/logDescription.test.ts
@@ -1,7 +1,7 @@
 import assert from "assert"
 import SimpleDB from "../../../src/class/SimpleDB.js"
 
-describe("logTable", () => {
+describe("logDescription", () => {
     let sdb: SimpleDB
     before(async function () {
         sdb = new SimpleDB()
@@ -9,17 +9,18 @@ describe("logTable", () => {
     after(async function () {
         await sdb.done()
     })
-    it("should log a table", async () => {
+
+    it("should log a description of the table", async () => {
         const table = sdb.newTable()
         await table.loadData("test/data/files/employees.csv")
-        await table.logTable()
+        await table.logDescription()
 
         // How to test?
         assert.deepStrictEqual(true, true)
     })
     it("should not throw an error when there is no table", async () => {
         const table = sdb.newTable()
-        await table.logTable()
+        await table.logDescription()
 
         // How to test?
         assert.deepStrictEqual(true, true)
