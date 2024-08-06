5dd5926c4f5f23b80914558e7ef9d60f9df5e3ad
Nael Shiab <nael.shiab@gmail.com>
2024-08-06 08:40:17 -0400

    insertTables now uses column names instead of column positions (#649)

--- test/unit/methods/insertTables.test.ts
+++ test/unit/methods/insertTables.test.ts
@@ -30,6 +30,27 @@ describe("insertTables", () => {
             { key1: 4, key2: "quatre" },
         ])
     })
+    it("should add rows from a table into another table even if the column order is not the same", async () => {
+        const table1 = sdb.newTable("table1")
+        await table1.loadData("test/data/files/data.json")
+
+        const table2 = sdb.newTable("table2")
+        await table2.loadData("test/data/files/data.json")
+        await table2.selectColumns(["key2", "key1"])
+
+        await table1.insertTables(table2)
+        const data = await table1.getData()
+        assert.deepStrictEqual(data, [
+            { key1: 1, key2: "un" },
+            { key1: 2, key2: "deux" },
+            { key1: 3, key2: "trois" },
+            { key1: 4, key2: "quatre" },
+            { key1: 1, key2: "un" },
+            { key1: 2, key2: "deux" },
+            { key1: 3, key2: "trois" },
+            { key1: 4, key2: "quatre" },
+        ])
+    })
     it("should add rows from multiple tables into another table", async () => {
         const table1 = sdb.newTable("table1")
         await table1.loadData("test/data/files/data.json")
