3bd8b26150f26288eb6d22d14b7cbbf6ff45b459
Nael Shiab <nael.shiab@gmail.com>
2024-08-26 13:44:31 -0400

    Fixing zScore

--- test/unit/methods/zScore.test.ts
+++ test/unit/methods/zScore.test.ts
@@ -37,25 +37,24 @@ describe("zScore", () => {
         const data = await table.getData()
 
         assert.deepStrictEqual(data, [
-            { name: "Evangeline", age: 21, ageZ: -1.2460801157839236 },
-            { name: "Amelia", age: 29, ageZ: -0.6922667309910687 },
-            { name: "Marie", age: 30, ageZ: -0.6230400578919618 },
-            { name: "Kiara", age: 31, ageZ: -0.5538133847928549 },
-            { name: "Isobel", age: 31, ageZ: -0.5538133847928549 },
-            { name: "Genevieve", age: 32, ageZ: -0.48458671169374806 },
-            { name: "Jane", age: 32, ageZ: -0.48458671169374806 },
-            { name: "Chloe", age: 33, ageZ: -0.4153600385946412 },
-            { name: "Philip", age: 33, ageZ: -0.4153600385946412 },
-            { name: "Morgan", age: 33, ageZ: -0.4153600385946412 },
-            { name: "Jeremy", age: 34, ageZ: -0.34613336549553436 },
-            { name: "Claudia", age: 35, ageZ: -0.27690669239642746 },
-            { name: "Sonny", age: 57, ageZ: 1.2460801157839236 },
-            { name: "Frazer", age: 64, ageZ: 1.7306668274776718 },
-            { name: "Sarah", age: 64, ageZ: 1.7306668274776718 },
-            { name: "Frankie", age: 65, ageZ: 1.7998935005767784 },
+            { name: "Evangeline", age: 21, ageZ: -1.2869460097256922 },
+            { name: "Amelia", age: 29, ageZ: -0.7149700054031624 },
+            { name: "Marie", age: 30, ageZ: -0.6434730048628461 },
+            { name: "Kiara", age: 31, ageZ: -0.5719760043225299 },
+            { name: "Isobel", age: 31, ageZ: -0.5719760043225299 },
+            { name: "Genevieve", age: 32, ageZ: -0.5004790037822137 },
+            { name: "Jane", age: 32, ageZ: -0.5004790037822137 },
+            { name: "Chloe", age: 33, ageZ: -0.42898200324189745 },
+            { name: "Philip", age: 33, ageZ: -0.42898200324189745 },
+            { name: "Morgan", age: 33, ageZ: -0.42898200324189745 },
+            { name: "Jeremy", age: 34, ageZ: -0.3574850027015812 },
+            { name: "Claudia", age: 35, ageZ: -0.28598800216126496 },
+            { name: "Sonny", age: 57, ageZ: 1.2869460097256922 },
+            { name: "Frazer", age: 64, ageZ: 1.787425013507906 },
+            { name: "Sarah", age: 64, ageZ: 1.787425013507906 },
+            { name: "Frankie", age: 65, ageZ: 1.858922014048222 },
         ])
     })
-
     it("should add a column with the zScore rounded to 3 decimals", async () => {
         const table = sdb.newTable()
         await table.loadArray([
@@ -85,22 +84,22 @@ describe("zScore", () => {
         const data = await table.getData()
 
         assert.deepStrictEqual(data, [
-            { name: "Evangeline", age: 21, ageSigma: -1.246 },
-            { name: "Amelia", age: 29, ageSigma: -0.692 },
-            { name: "Marie", age: 30, ageSigma: -0.623 },
-            { name: "Kiara", age: 31, ageSigma: -0.554 },
-            { name: "Isobel", age: 31, ageSigma: -0.554 },
-            { name: "Genevieve", age: 32, ageSigma: -0.485 },
-            { name: "Jane", age: 32, ageSigma: -0.485 },
-            { name: "Chloe", age: 33, ageSigma: -0.415 },
-            { name: "Philip", age: 33, ageSigma: -0.415 },
-            { name: "Morgan", age: 33, ageSigma: -0.415 },
-            { name: "Jeremy", age: 34, ageSigma: -0.346 },
-            { name: "Claudia", age: 35, ageSigma: -0.277 },
-            { name: "Sonny", age: 57, ageSigma: 1.246 },
-            { name: "Frazer", age: 64, ageSigma: 1.731 },
-            { name: "Sarah", age: 64, ageSigma: 1.731 },
-            { name: "Frankie", age: 65, ageSigma: 1.8 },
+            { name: "Evangeline", age: 21, ageSigma: -1.287 },
+            { name: "Amelia", age: 29, ageSigma: -0.715 },
+            { name: "Marie", age: 30, ageSigma: -0.643 },
+            { name: "Kiara", age: 31, ageSigma: -0.572 },
+            { name: "Isobel", age: 31, ageSigma: -0.572 },
+            { name: "Genevieve", age: 32, ageSigma: -0.5 },
+            { name: "Jane", age: 32, ageSigma: -0.5 },
+            { name: "Chloe", age: 33, ageSigma: -0.429 },
+            { name: "Philip", age: 33, ageSigma: -0.429 },
+            { name: "Morgan", age: 33, ageSigma: -0.429 },
+            { name: "Jeremy", age: 34, ageSigma: -0.357 },
+            { name: "Claudia", age: 35, ageSigma: -0.286 },
+            { name: "Sonny", age: 57, ageSigma: 1.287 },
+            { name: "Frazer", age: 64, ageSigma: 1.787 },
+            { name: "Sarah", age: 64, ageSigma: 1.787 },
+            { name: "Frankie", age: 65, ageSigma: 1.859 },
         ])
     })
     it("should add a column with the zScore rounded to 3 decimals and with a category", async () => {
@@ -135,22 +134,22 @@ describe("zScore", () => {
         const data = await table.getData()
 
         assert.deepStrictEqual(data, [
-            { name: "Philip", age: 33, gender: "Man", ageSigma: -0.883 },
-            { name: "Jeremy", age: 34, gender: "Man", ageSigma: -0.82 },
-            { name: "Sonny", age: 57, gender: "Man", ageSigma: 0.631 },
-            { name: "Frazer", age: 64, gender: "Man", ageSigma: 1.072 },
-            { name: "Evangeline", age: 21, gender: "Woman", ageSigma: -1.127 },
-            { name: "Amelia", age: 29, gender: "Woman", ageSigma: -0.539 },
-            { name: "Marie", age: 30, gender: "Woman", ageSigma: -0.466 },
-            { name: "Kiara", age: 31, gender: "Woman", ageSigma: -0.392 },
-            { name: "Isobel", age: 31, gender: "Woman", ageSigma: -0.392 },
-            { name: "Genevieve", age: 32, gender: "Woman", ageSigma: -0.319 },
-            { name: "Jane", age: 32, gender: "Woman", ageSigma: -0.319 },
-            { name: "Chloe", age: 33, gender: "Woman", ageSigma: -0.245 },
-            { name: "Morgan", age: 33, gender: "Woman", ageSigma: -0.245 },
-            { name: "Claudia", age: 35, gender: "Woman", ageSigma: -0.098 },
-            { name: "Sarah", age: 64, gender: "Woman", ageSigma: 2.034 },
-            { name: "Frankie", age: 65, gender: "Woman", ageSigma: 2.108 },
+            { name: "Philip", age: 33, gender: "Man", ageSigma: -1.02 },
+            { name: "Jeremy", age: 34, gender: "Man", ageSigma: -0.947 },
+            { name: "Sonny", age: 57, gender: "Man", ageSigma: 0.728 },
+            { name: "Frazer", age: 64, gender: "Man", ageSigma: 1.238 },
+            { name: "Evangeline", age: 21, gender: "Woman", ageSigma: -1.178 },
+            { name: "Amelia", age: 29, gender: "Woman", ageSigma: -0.563 },
+            { name: "Marie", age: 30, gender: "Woman", ageSigma: -0.486 },
+            { name: "Kiara", age: 31, gender: "Woman", ageSigma: -0.41 },
+            { name: "Isobel", age: 31, gender: "Woman", ageSigma: -0.41 },
+            { name: "Genevieve", age: 32, gender: "Woman", ageSigma: -0.333 },
+            { name: "Jane", age: 32, gender: "Woman", ageSigma: -0.333 },
+            { name: "Chloe", age: 33, gender: "Woman", ageSigma: -0.256 },
+            { name: "Morgan", age: 33, gender: "Woman", ageSigma: -0.256 },
+            { name: "Claudia", age: 35, gender: "Woman", ageSigma: -0.102 },
+            { name: "Sarah", age: 64, gender: "Woman", ageSigma: 2.125 },
+            { name: "Frankie", age: 65, gender: "Woman", ageSigma: 2.202 },
         ])
     })
 })
