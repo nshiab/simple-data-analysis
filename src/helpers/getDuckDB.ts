2c9e5a4381197cae9a6ab2a87b177a581a477bc6
Nael Shiab <nael.shiab@gmail.com>
2024-09-09 20:19:43 -0400

    updating wasm version

--- src/helpers/getDuckDB.ts
+++ src/helpers/getDuckDB.ts
@@ -1,28 +1,21 @@
 import * as duckdb from "@duckdb/duckdb-wasm"
 import { AsyncDuckDB } from "@duckdb/duckdb-wasm"
 
-const SilentLogger = { log: () => {} }
-
-async function browserWorkerBundle() {
-    const allBundles = duckdb.getJsDelivrBundles()
-    const bundle = await duckdb.selectBundle(allBundles)
-    const mainWorker = bundle.mainWorker
-    if (mainWorker) {
-        const worker = await duckdb.createWorker(mainWorker)
-        return { bundle, worker }
-    } else {
-        throw Error(`No mainWorker: ${mainWorker}`)
-    }
-}
-
 export default async function getDuckDB(): Promise<{
     db: AsyncDuckDB
     worker: Worker
 }> {
-    const { worker, bundle } = await browserWorkerBundle()
-    const logger = SilentLogger
-    const db = new AsyncDuckDB(logger, worker)
+    const bundle = await duckdb.selectBundle(duckdb.getJsDelivrBundles())
+    const worker_url = URL.createObjectURL(
+        new Blob([`importScripts("${bundle.mainWorker!}");`], {
+            type: "text/javascript",
+        })
+    )
+    const worker = new Worker(worker_url)
+    const logger = new duckdb.ConsoleLogger()
+    const db = new duckdb.AsyncDuckDB(logger, worker)
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker)
+    URL.revokeObjectURL(worker_url)
     await db.open({
         path: ":memory:",
         query: {
