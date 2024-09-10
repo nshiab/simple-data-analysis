c319bf7565557c72996b75da01f21e90c0aae36b
Nael Shiab <nael.shiab@gmail.com>
2024-09-09 20:24:03 -0400

    Silent logger for wasm version

--- src/helpers/getDuckDB.ts
+++ src/helpers/getDuckDB.ts
@@ -12,8 +12,9 @@ export default async function getDuckDB(): Promise<{
         })
     )
     const worker = new Worker(worker_url)
-    const logger = new duckdb.ConsoleLogger()
-    const db = new duckdb.AsyncDuckDB(logger, worker)
+    // const logger = new duckdb.ConsoleLogger()
+    const SilentLogger = { log: () => {} }
+    const db = new duckdb.AsyncDuckDB(SilentLogger, worker)
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker)
     URL.revokeObjectURL(worker_url)
     await db.open({
