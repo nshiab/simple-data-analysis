a524beee04fd4431244ec49439b43762595c4acf
Nael Shiab <nael.shiab@gmail.com>
2024-10-12 10:09:43 -0400

    fixing tests for CLI

--- test/unit/methods/loadDataFromDirectory.test.ts
+++ test/unit/methods/loadDataFromDirectory.test.ts
@@ -19,24 +19,31 @@ Deno.test("should load data from a directory", async () => {
     unifyColumns: true,
   });
 
-  const data = await table.getData();
+  const data = await table.getData() as {
+    key1: number;
+    key2: string;
+    key3: null | string;
+  }[];
 
-  assertEquals(data, [
-    { key1: 9, key2: "neuf", key3: null },
-    { key1: 10, key2: "dix", key3: null },
-    { key1: 11, key2: "onze", key3: null },
-    { key1: 5, key2: "cinq", key3: null },
-    { key1: 6, key2: "six", key3: null },
-    { key1: 7, key2: "sept", key3: null },
-    { key1: 8, key2: "huit", key3: null },
-    { key1: 1, key2: "un", key3: null },
-    { key1: 2, key2: "deux", key3: null },
-    { key1: 3, key2: "trois", key3: null },
-    { key1: 4, key2: "quatre", key3: null },
-    { key1: 9, key2: "neuf", key3: "nine" },
-    { key1: 10, key2: "dix", key3: "ten" },
-    { key1: 11, key2: "onze", key3: "eleven" },
-  ]);
+  assertEquals(
+    data.sort((a, b) => a.key1 - b.key1),
+    [
+      { key1: 9, key2: "neuf", key3: null },
+      { key1: 10, key2: "dix", key3: null },
+      { key1: 11, key2: "onze", key3: null },
+      { key1: 5, key2: "cinq", key3: null },
+      { key1: 6, key2: "six", key3: null },
+      { key1: 7, key2: "sept", key3: null },
+      { key1: 8, key2: "huit", key3: null },
+      { key1: 1, key2: "un", key3: null },
+      { key1: 2, key2: "deux", key3: null },
+      { key1: 3, key2: "trois", key3: null },
+      { key1: 4, key2: "quatre", key3: null },
+      { key1: 9, key2: "neuf", key3: "nine" },
+      { key1: 10, key2: "dix", key3: "ten" },
+      { key1: 11, key2: "onze", key3: "eleven" },
+    ].sort((a, b) => a.key1 - b.key1),
+  );
 });
 Deno.test("should load data from a directory even when the path doesn't have '/' at the end", async () => {
   const table = sdb.newTable();
@@ -44,24 +51,31 @@ Deno.test("should load data from a directory even when the path doesn't have '/'
     unifyColumns: true,
   });
 
-  const data = await table.getData();
+  const data = await table.getData() as {
+    key1: number;
+    key2: string;
+    key3: null | string;
+  }[];
 
-  assertEquals(data, [
-    { key1: 9, key2: "neuf", key3: null },
-    { key1: 10, key2: "dix", key3: null },
-    { key1: 11, key2: "onze", key3: null },
-    { key1: 5, key2: "cinq", key3: null },
-    { key1: 6, key2: "six", key3: null },
-    { key1: 7, key2: "sept", key3: null },
-    { key1: 8, key2: "huit", key3: null },
-    { key1: 1, key2: "un", key3: null },
-    { key1: 2, key2: "deux", key3: null },
-    { key1: 3, key2: "trois", key3: null },
-    { key1: 4, key2: "quatre", key3: null },
-    { key1: 9, key2: "neuf", key3: "nine" },
-    { key1: 10, key2: "dix", key3: "ten" },
-    { key1: 11, key2: "onze", key3: "eleven" },
-  ]);
+  assertEquals(
+    data.sort((a, b) => a.key1 - b.key1),
+    [
+      { key1: 9, key2: "neuf", key3: null },
+      { key1: 10, key2: "dix", key3: null },
+      { key1: 11, key2: "onze", key3: null },
+      { key1: 5, key2: "cinq", key3: null },
+      { key1: 6, key2: "six", key3: null },
+      { key1: 7, key2: "sept", key3: null },
+      { key1: 8, key2: "huit", key3: null },
+      { key1: 1, key2: "un", key3: null },
+      { key1: 2, key2: "deux", key3: null },
+      { key1: 3, key2: "trois", key3: null },
+      { key1: 4, key2: "quatre", key3: null },
+      { key1: 9, key2: "neuf", key3: "nine" },
+      { key1: 10, key2: "dix", key3: "ten" },
+      { key1: 11, key2: "onze", key3: "eleven" },
+    ].sort((a, b) => a.key1 - b.key1),
+  );
 });
 Deno.test("should load data from a directory with a limit option", async () => {
   const table = sdb.newTable();
