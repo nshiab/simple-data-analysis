e96a0233e48101053239fc85fd87963777b2688a
Nael Shiab <nael.shiab@gmail.com>
2024-10-24 10:34:20 -0400

    Method clone column now accepts column names with spaces

--- test/unit/methods/cloneColumn.test.ts
+++ test/unit/methods/cloneColumn.test.ts
@@ -16,7 +16,21 @@ Deno.test("should clone a column", async () => {
 
   await sdb.done();
 });
+Deno.test("should clone a column with spaces in its name", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("data");
+  await table.loadArray([{ "first name": "nael", "last name": "shiab" }]);
 
+  await table.cloneColumn("first name", "first name cloned");
+
+  const data = await table.getData();
+
+  assertEquals(data, [
+    { "first name": "nael", "last name": "shiab", "first name cloned": "nael" },
+  ]);
+
+  await sdb.done();
+});
 Deno.test("should clone a column with geometries and keep the projection", async () => {
   const sdb = new SimpleDB();
   const table = sdb.newTable("data");
