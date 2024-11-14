3624fe1c8d1e52544317a629f8e257ad2aa0f8b0
Nael Shiab <nael.shiab@gmail.com>
2024-11-13 20:44:34 -0500

    Formatting

--- src/helpers/getProjectionParquet.ts
+++ src/helpers/getProjectionParquet.ts
@@ -3,36 +3,36 @@ import mergeOptions from "./mergeOptions.ts";
 import queryDB from "./queryDB.ts";
 
 export default async function getProjectionParquet(
-    simpleWebTable: SimpleWebTable,
-    file: string,
+  simpleWebTable: SimpleWebTable,
+  file: string,
 ) {
-    const queryResult = await queryDB(
-        simpleWebTable,
-        `SELECT * FROM parquet_kv_metadata('${file}');`,
-        mergeOptions(simpleWebTable, {
-            table: simpleWebTable.name,
-            method: "getProjectionParquet()",
-            parameters: { file },
-            returnDataFrom: "query",
-        }),
+  const queryResult = await queryDB(
+    simpleWebTable,
+    `SELECT * FROM parquet_kv_metadata('${file}');`,
+    mergeOptions(simpleWebTable, {
+      table: simpleWebTable.name,
+      method: "getProjectionParquet()",
+      parameters: { file },
+      returnDataFrom: "query",
+    }),
+  );
+
+  if (!queryResult) {
+    throw new Error(
+      `Could not get metadata from parquet file: ${file}`,
     );
+  }
 
-    if (!queryResult) {
-        throw new Error(
-            `Could not get metadata from parquet file: ${file}`,
-        );
-    }
+  const projection = queryResult.find((d) => {
+    return d.key?.toString() === "projections";
+  });
 
-    const projection = queryResult.find((d) => {
-        return d.key?.toString() === "projections";
-    });
-
-    if (!projection || projection.value === null) {
-        console.warn(
-            `\nCould not get projection from parquet file: ${file}\n`,
-        );
-        return {};
-    } else {
-        return JSON.parse(projection.value.toString());
-    }
+  if (!projection || projection.value === null) {
+    console.warn(
+      `\nCould not get projection from parquet file: ${file}\n`,
+    );
+    return {};
+  } else {
+    return JSON.parse(projection.value.toString());
+  }
 }
