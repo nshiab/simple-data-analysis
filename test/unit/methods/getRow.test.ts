56bba11e7ba0244dedbe680f08be995317fb7134
Nael Shiab <nael.shiab@gmail.com>
2024-11-28 14:53:02 -0500

    New method .getRow()

---
+++ test/unit/methods/getRow.test.ts
@@ -0,0 +1,19 @@
+import { assertEquals } from "jsr:@std/assert";
+import SimpleDB from "../../../src/class/SimpleDB.ts";
+
+Deno.test("should return a specific row", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("data");
+  await table.loadData("test/data/files/employees.csv");
+  const data = await table.getRow(`Name === 'Grant, Douglas'`);
+
+  assertEquals(data, {
+    Name: "Grant, Douglas",
+    "Hire date": "13-JAN-08",
+    Job: "Clerk",
+    Salary: "NaN",
+    "Department or unit": "50",
+    "End-of_year-BONUS?": "23,39%",
+  });
+  await sdb.done();
+});
