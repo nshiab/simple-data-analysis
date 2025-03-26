ecab1ecb05e65397db00ff4046fbe1ffb1297dda
Nael Shiab <nael.shiab@gmail.com>
2025-03-26 13:39:16 -0400

    removeMissing accepts column names with special characters

--- src/methods/removeMissingQuery.ts
+++ src/methods/removeMissingQuery.ts
@@ -12,45 +12,45 @@ export default function removeMissingQuery(
 ) {
   let query = `CREATE OR REPLACE TABLE "${table}" AS SELECT ${
     allColumns
-      .map((d) => `${d}`)
+      .map((d) => `"${d}"`)
       .join(", ")
   } FROM "${table}"
         WHERE`;
 
   if (options.invert) {
     for (let i = 0; i < columns.length; i++) {
-      query += `\n${columns[i]} IS NULL OR`;
+      query += `\n"${columns[i]}" IS NULL OR`;
       if (options.missingValues) {
         for (const otherMissingValue of options.missingValues) {
           if (
             typeof otherMissingValue === "string" &&
             types[columns[i]] === "VARCHAR"
           ) {
-            query += `\n${columns[i]} = '${otherMissingValue}' OR`;
+            query += `\n"${columns[i]}" = '${otherMissingValue}' OR`;
           } else if (
             typeof otherMissingValue === "number" &&
             ["BIGINT", "DOUBLE"].includes(types[columns[i]])
           ) {
-            query += `\n${columns[i]} = ${otherMissingValue} OR`;
+            query += `\n"${columns[i]}" = ${otherMissingValue} OR`;
           }
         }
       }
     }
   } else {
     for (let i = 0; i < columns.length; i++) {
-      query += `\n${columns[i]} IS NOT NULL AND`;
+      query += `\n"${columns[i]}" IS NOT NULL AND`;
       if (options.missingValues) {
         for (const otherMissingValue of options.missingValues) {
           if (
             typeof otherMissingValue === "string" &&
             types[columns[i]] === "VARCHAR"
           ) {
-            query += `\n${columns[i]} != '${otherMissingValue}' AND`;
+            query += `\n"${columns[i]}" != '${otherMissingValue}' AND`;
           } else if (
             typeof otherMissingValue === "number" &&
             ["BIGINT", "DOUBLE"].includes(types[columns[i]])
           ) {
-            query += `\n${columns[i]} != ${otherMissingValue} AND`;
+            query += `\n"${columns[i]}" != ${otherMissingValue} AND`;
           }
         }
       }
