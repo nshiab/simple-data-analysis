d389f6998168ab356ac23958c6dc291921a5efe7
Nael Shiab <nael.shiab@gmail.com>
2025-03-28 16:09:46 -0400

    Better handling of === in SQL query

--- src/helpers/cleanSQL.ts
+++ src/helpers/cleanSQL.ts
@@ -1,22 +1,25 @@
 export default function cleanSQL(query: string) {
-  if (
-    query.includes("ALTER TABLE") && query.includes("UPDATE") &&
-    query.includes("SET")
-  ) {
-    return query;
-  }
-
-  const cleaned = query
+  // First pass
+  let cleaned = query
     .replace(/ && /g, " AND ")
     .replace(/ & /g, " AND ")
     .replace(/ \|\| /g, " OR ")
     .replace(/ \| /g, " OR ")
-    .replace(/ null(?=\s|$)/g, " NULL") // space after or end of string
     .replace(/ === /g, " = ")
     .replace(/ == /g, " = ")
-    .replace(/ !== /g, " != ")
-    .replace(/ != NULL/g, " NOT NULL")
-    .replace(/ = NULL/g, " IS NULL");
+    .replace(/ !== /g, " != ");
+
+  if (
+    query.includes("ALTER TABLE") && query.includes("UPDATE") &&
+    query.includes("SET")
+  ) {
+    // We pass
+  } else {
+    // We do a second pass
+    cleaned = query.replace(/ null(?=\s|$)/g, " NULL") // space after or end of string
+      .replace(/ != NULL/g, " NOT NULL")
+      .replace(/ = NULL/g, " IS NULL");
+  }
 
   return cleaned;
 }
