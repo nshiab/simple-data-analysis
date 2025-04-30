aa6327c17b9d59a21d76d0afc0fed177b1f38aba
Nael Shiab <nael.shiab@gmail.com>
2025-04-30 16:06:59 -0400

    New option cache for aiRowByRow and aiQuery

--- test/unit/methods/aiQuery.test.ts
+++ test/unit/methods/aiQuery.test.ts
@@ -1,9 +1,14 @@
 import "jsr:@std/dotenv/load";
 import { assertEquals } from "jsr:@std/assert";
 import SimpleDB from "../../../src/class/SimpleDB.ts";
+import { existsSync, rmSync } from "node:fs";
 
 const aiKey = Deno.env.get("AI_KEY");
 if (typeof aiKey === "string" && aiKey !== "") {
+  if (existsSync("./.journalism")) {
+    rmSync("./.journalism", { recursive: true });
+  }
+
   Deno.test("should update a table with natural language", async () => {
     const sdb = new SimpleDB();
     const table = sdb.newTable("data");
@@ -12,6 +17,40 @@ if (typeof aiKey === "string" && aiKey !== "") {
 
     await table.aiQuery(
       `I want the average temperature for each city with two decimals.`,
+    );
+
+    await table.logTable();
+
+    // Just to make sure it doesn't crash for now
+    assertEquals(true, true);
+    await sdb.done();
+  });
+  Deno.test("should update a table with natural language with cache", async () => {
+    const sdb = new SimpleDB();
+    const table = sdb.newTable("data");
+    await table.loadData("test/data/files/dailyTemperatures.csv");
+    await table.renameColumns({ t: "temperature", "id": "city" });
+
+    await table.aiQuery(
+      `I want the average temperature for each city with two decimals.`,
+      { cache: true, cacheVerbose: true },
+    );
+
+    await table.logTable();
+
+    // Just to make sure it doesn't crash for now
+    assertEquals(true, true);
+    await sdb.done();
+  });
+  Deno.test("should update a table with natural language with query returned from cache", async () => {
+    const sdb = new SimpleDB();
+    const table = sdb.newTable("data");
+    await table.loadData("test/data/files/dailyTemperatures.csv");
+    await table.renameColumns({ t: "temperature", "id": "city" });
+
+    await table.aiQuery(
+      `I want the average temperature for each city with two decimals.`,
+      { cache: true, cacheVerbose: true },
     );
 
     await table.logTable();
--- test/unit/methods/aiRowByRow.test.ts
+++ test/unit/methods/aiRowByRow.test.ts
@@ -1,9 +1,14 @@
 import "jsr:@std/dotenv/load";
 import { assertEquals } from "jsr:@std/assert";
 import SimpleDB from "../../../src/class/SimpleDB.ts";
+import { existsSync, rmSync } from "node:fs";
 
 const aiKey = Deno.env.get("AI_KEY");
 if (typeof aiKey === "string" && aiKey !== "") {
+  if (existsSync("./.journalism")) {
+    rmSync("./.journalism", { recursive: true });
+  }
+
   Deno.test("should iterate over rows with a prompt", async () => {
     const sdb = new SimpleDB();
     const table = sdb.newTable("data");
@@ -26,6 +31,52 @@ if (typeof aiKey === "string" && aiKey !== "") {
     ]);
     await sdb.done();
   });
+  Deno.test("should iterate over rows with a prompt and cache", async () => {
+    const sdb = new SimpleDB();
+    const table = sdb.newTable("data");
+    await table.loadArray([
+      { "city": "Marrakech" },
+      { "city": "Kyoto" },
+      { "city": "Auckland" },
+    ]);
+    await table.aiRowByRow(
+      "city",
+      "country",
+      `Give me the country of the city.`,
+      { cache: true, cacheVerbose: true },
+    );
+    const data = await table.getData();
+
+    assertEquals(data, [
+      { city: "Marrakech", country: "Morocco" },
+      { city: "Kyoto", country: "Japan" },
+      { city: "Auckland", country: "New Zealand" },
+    ]);
+    await sdb.done();
+  });
+  Deno.test("should iterate over rows with a prompt and return from cache", async () => {
+    const sdb = new SimpleDB();
+    const table = sdb.newTable("data");
+    await table.loadArray([
+      { "city": "Marrakech" },
+      { "city": "Kyoto" },
+      { "city": "Auckland" },
+    ]);
+    await table.aiRowByRow(
+      "city",
+      "country",
+      `Give me the country of the city.`,
+      { cache: true, cacheVerbose: true },
+    );
+    const data = await table.getData();
+
+    assertEquals(data, [
+      { city: "Marrakech", country: "Morocco" },
+      { city: "Kyoto", country: "Japan" },
+      { city: "Auckland", country: "New Zealand" },
+    ]);
+    await sdb.done();
+  });
   Deno.test("should iterate over rows with a prompt and be verbose", async () => {
     const sdb = new SimpleDB();
     const table = sdb.newTable("data");
@@ -38,7 +89,7 @@ if (typeof aiKey === "string" && aiKey !== "") {
       "city",
       "country",
       `Give me the country of the city.`,
-      { verbose: true },
+      { cacheVerbose: true },
     );
     const data = await table.getData();
 
