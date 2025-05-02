f3bb49ce8752cf6576eec85b737dab7db4a1d2a3
Nael Shiab <nael.shiab@gmail.com>
2025-05-02 10:10:03 -0400

    Improving logs for aiQuery

--- src/methods/aiQuery.ts
+++ src/methods/aiQuery.ts
@@ -17,22 +17,15 @@ export default async function aiQuery(
   const p =
     `I have a SQL table named "${simpleTable.name}". The data is already in it with these columns:\n${
       JSON.stringify(await simpleTable.getTypes(), undefined, 2)
-    }\nI want you to give me a SQL query to do this:\n- ${prompt}\nThe query must replace the existing "${simpleTable.name}" table with 'CREATE OR REPLACE TABLE "${simpleTable.name}"'. Return just the query, nothing else.`;
+    }\nI want you to give me a SQL query to do this:\n- ${prompt}\nThe query must replace the existing "${simpleTable.name}" table. This means the the query must start with 'CREATE OR REPLACE TABLE "${simpleTable.name}"...'. Return just the query, nothing else.`;
 
   if (options.verbose) {
     console.log("\naiQuery()");
-    console.log("\nPrompt:");
-    console.log(p);
   }
 
   // Types could be improved
   let query = await askAI(p, options) as unknown as string;
   query = query.replace("```sql", "").replace("```", "").trim();
-
-  if (options.verbose) {
-    console.log("\nResponse:");
-    console.log(query);
-  }
 
   await simpleTable.sdb.customQuery(query);
 }
