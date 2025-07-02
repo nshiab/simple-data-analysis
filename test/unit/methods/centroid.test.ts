0e60b9ca629fe4c32509790183a0b011a503bbb3
Nael Shiab <nael.shiab@gmail.com>
2025-07-02 11:29:55 -0400

    Fix tests

--- test/unit/methods/centroid.test.ts
+++ test/unit/methods/centroid.test.ts
@@ -10,6 +10,7 @@ Deno.test("should compute the centroids", async () => {
   await table.centroid("centroid");
 
   await table.selectColumns(["nameEnglish", "centroid"]);
+  await table.reducePrecision(4);
 
   const data = await table.getGeoData("centroid");
 
@@ -21,8 +22,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -60.513931135839655,
-            52.89367552075254,
+            -60.5139,
+            52.8937,
           ],
         },
         "properties": {
@@ -34,8 +35,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -63.23603949323203,
-            46.38172009310323,
+            -63.236,
+            46.3817,
           ],
         },
         "properties": {
@@ -47,8 +48,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -63.277862438654495,
-            45.15730079129712,
+            -63.2779,
+            45.1573,
           ],
         },
         "properties": {
@@ -60,8 +61,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -66.39159809401114,
-            46.62631203489651,
+            -66.3916,
+            46.6263,
           ],
         },
         "properties": {
@@ -73,8 +74,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -71.79225358385656,
-            53.39829619196409,
+            -71.7923,
+            53.3983,
           ],
         },
         "properties": {
@@ -86,8 +87,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -86.04446366485669,
-            50.46635291129,
+            -86.0445,
+            50.4664,
           ],
         },
         "properties": {
@@ -99,8 +100,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -97.42859847372449,
-            54.930503752005336,
+            -97.4286,
+            54.9305,
           ],
         },
         "properties": {
@@ -112,8 +113,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -105.8881135746785,
-            54.41637207356226,
+            -105.8881,
+            54.4164,
           ],
         },
         "properties": {
@@ -125,8 +126,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -114.50881912067959,
-            55.17080382362937,
+            -114.5088,
+            55.1708,
           ],
         },
         "properties": {
@@ -138,8 +139,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -124.71820632563735,
-            54.77769445637783,
+            -124.7182,
+            54.7777,
           ],
         },
         "properties": {
@@ -151,8 +152,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -135.51309195114175,
-            63.63254927852782,
+            -135.5131,
+            63.6325,
           ],
         },
         "properties": {
@@ -164,8 +165,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -118.99109927571025,
-            66.36800799277671,
+            -118.9911,
+            66.368,
           ],
         },
         "properties": {
@@ -177,8 +178,8 @@ Deno.test("should compute the centroids", async () => {
         "geometry": {
           "type": "Point",
           "coordinates": [
-            -88.66269882878774,
-            71.19002776226056,
+            -88.6627,
+            71.19,
           ],
         },
         "properties": {
