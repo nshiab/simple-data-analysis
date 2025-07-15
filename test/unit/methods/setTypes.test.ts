dff0cda93d8f63edb389056182fc1778b8a9e883
Nael Shiab <nael.shiab@gmail.com>
2025-07-15 15:48:00 -0400

    Better handling of column names with setTypes method

--- test/unit/methods/setTypes.test.ts
+++ test/unit/methods/setTypes.test.ts
@@ -25,3 +25,29 @@ Deno.test("should create a new SimpleTable with geometry in types", async () => 
   );
   await sdb.done();
 });
+
+Deno.test("should create a new SimpleTable with types and column names containing spaces", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("data");
+  await table.setTypes({ "first name": "string", age: "number" });
+  const types = await table.getTypes();
+  assertEquals(types, { "first name": "VARCHAR", age: "DOUBLE" });
+  await sdb.done();
+});
+
+Deno.test("should create a new SimpleTable with types and column names with special uses", async () => {
+  const sdb = new SimpleDB();
+  const table = sdb.newTable("data");
+  await table.setTypes({
+    "first name": "string",
+    age: "number",
+    Group: "string",
+  });
+  const types = await table.getTypes();
+  assertEquals(types, {
+    "first name": "VARCHAR",
+    age: "DOUBLE",
+    Group: "VARCHAR",
+  });
+  await sdb.done();
+});
