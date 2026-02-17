8b93803163c53fe6f081ba6ba84c4247ca14e977
nshiab <nael.shiab@gmail.com>
2026-02-17 11:30:35 -0500

    Add verbose logging for timing in aiRAG function and update tests to enable verbose option

--- src/methods/aiRAG.ts
+++ src/methods/aiRAG.ts
@@ -1,5 +1,6 @@
 import { askAI } from "@nshiab/journalism-ai";
 import type SimpleTable from "../class/SimpleTable.ts";
+import { prettyDuration } from "@nshiab/journalism-format";
 
 export async function aiRAG(
   table: SimpleTable,
@@ -19,6 +20,19 @@ export async function aiRAG(
     ollamaEmbeddings?: boolean;
   } = {},
 ) {
+  const times = {
+    embeddingStart: 0,
+    embeddingEnd: 0,
+    vectorSearchStart: 0,
+    vectorSearchEnd: 0,
+    llmStart: 0,
+    llmEnd: 0,
+  };
+
+  if (options.verbose) {
+    times.embeddingStart = Date.now();
+  }
+
   const embeddingColumn = `${column}_embeddings`;
 
   options.cache
@@ -38,6 +52,11 @@ export async function aiRAG(
       model: options.embeddingsModel,
     });
 
+  if (options.verbose) {
+    times.embeddingEnd = Date.now();
+    times.vectorSearchStart = Date.now();
+  }
+
   const tempTable = await table.aiVectorSimilarity(
     query,
     embeddingColumn,
@@ -49,11 +68,20 @@ export async function aiRAG(
     },
   );
 
+  if (options.verbose) {
+    times.vectorSearchEnd = Date.now();
+    await tempTable.logTable();
+  }
+
   const retrievedData = await tempTable.getValues(column);
 
   await tempTable.removeTable();
 
-  return askAI(
+  if (options.verbose) {
+    times.llmStart = Date.now();
+  }
+
+  const response = await askAI(
     `Answer the following question:
 - ${query}.
 
@@ -75,4 +103,17 @@ ${retrievedData.join("\n\n---\n\n")}`,
       model: options.model,
     },
   ) as Promise<string>;
+
+  if (options.verbose) {
+    times.llmEnd = Date.now();
+    console.log(
+      `\nRAG process times:\n- Embedding: ${
+        prettyDuration(times.embeddingStart, { end: times.embeddingEnd })
+      }\n- Vector Search: ${
+        prettyDuration(times.vectorSearchStart, { end: times.vectorSearchEnd })
+      }\n- LLM: ${prettyDuration(times.llmStart, { end: times.llmEnd })}\n`,
+    );
+  }
+
+  return response;
 }
